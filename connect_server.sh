13b7cf5978bcf4c418d2dfe2706381d47b026831
Prussia <prussia.hu@gmail.com>
2016-12-17 09:58:23 +0800

    updates

--- connect_server.sh
+++ connect_server.sh
@@ -1,3 +1,3 @@
-ssh -i 'c:\Users\%USER%\.ssh\KeyPair_Tokyo2.pem' ubuntu@54.238.139.252
+ssh -i 'c:\Users\%USER%\.ssh\KeyPair_Tokyo2.pem' ubuntu@54.238.176.106
 
-ssh -i '/Users/$USER/.ssh/KeyPair_Tokyo2.pem' ubuntu@54.238.139.252
+ssh -i '/Users/$USER/.ssh/KeyPair_Tokyo2.pem' ubuntu@54.238.176.106
