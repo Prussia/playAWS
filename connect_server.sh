e1df762d2c4dbdb78a574a5c2621d7c5b0647b48
Prussia <prussia.hu@gmail.com>
2017-01-02 12:05:17 +0800

    update for shell

--- connect_server.sh
+++ connect_server.sh
@@ -1,3 +1,14 @@
-ssh -i 'c:\Users\%USER%\.ssh\KeyPair_Tokyo2.pem' ubuntu@54.238.176.106
+SET MYKEY='c:\Users\'%USER%'\.ssh\KeyPair_Tokyo2.pem'
+SET AWS_TOKYO_SERVER=ubuntu@54.238.176.106
 
-ssh -i '/Users/$USER/.ssh/KeyPair_Tokyo2.pem' ubuntu@54.238.176.106
+ssh -i %MYKEY% %AWS_TOKYO_SERVER%
+#ssh -i 'c:\Users\%USER%\.ssh\KeyPair_Tokyo2.pem' ubuntu@54.238.176.106
+
+
+#Mac or Linux
+#
+MYKEY='/Users/'$USER'/.ssh/KeyPair_Tokyo2.pem'
+AWS_TOKYO_SERVER=ubuntu@54.238.176.106
+sudo ssh -i $MYKEY $AWS_TOKYO_SERVER
+
+# ssh -i '/Users/$USER/.ssh/KeyPair_Tokyo2.pem' ubuntu@54.238.176.106
