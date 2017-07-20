44a5d1ed2506635a1e92537e5edc6ff18cf0fa90
Prussia <prussia.hu@pwc.com>
2017-07-20 14:21:57 +0800

    Update

--- 2007/connect_server_2007.sh
+++ 2007/connect_server_2007.sh
@@ -1,14 +1,14 @@
-SET MYKEY='c:\Users\'%USER%'\.ssh\KeyPair_Tokyo2007.pem'
+SET MYKEY='c:\Users\'%USER%'\.ssh\keypair2007.pem'
 SET AWS_TOKYO_SERVER=ubuntu@52.192.95.182
 
 ssh -i %MYKEY% %AWS_TOKYO_SERVER%
-#ssh -i 'c:\Users\%USER%\.ssh\KeyPair_Tokyo2007.pem' ubuntu@54.238.176.106
+#ssh -i 'c:\Users\%USER%\.ssh\keypair2007.pem' ubuntu@54.238.176.106
 
 
 #Mac or Linux
 #
-MYKEY='/Users/'$USER'/.ssh/KeyPair_Tokyo2007.pem'
+MYKEY='/Users/'$USER'/.ssh/keypair2007.pem'
 AWS_TOKYO_SERVER=52.192.95.182
 sudo ssh -i $MYKEY $AWS_TOKYO_SERVER
 
-# ssh -i '/Users/$USER/.ssh/KeyPair_Tokyo2007.pem' ubuntu@54.238.176.106
+# ssh -i '/Users/$USER/.ssh/keypair2007.pem' 52.192.95.182
