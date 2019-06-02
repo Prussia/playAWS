65dfe3c0e5c7871d2eaadfd64a26fffd295d959c
Prussia <prussia.hu@gmail.com>
2019-06-02 15:21:24 +0800

    Update create_container.sh

--- create_container.sh
+++ create_container.sh
@@ -2,12 +2,8 @@
 
 export SSPASSWORD=
 
-docker run -d -p 9001:9001 oddrationale/docker-shadowsocks -s 0.0.0.0 -p 9001 -k $SSPASSWORD -m aes-256-cfb
+docker run -d --name container1 -p 9001:9001 oddrationale/docker-shadowsocks -s 0.0.0.0 -p 9001 -k $SSPASSWORD -m aes-256-cfb
 
-docker run -d -p 9002:9002 oddrationale/docker-shadowsocks -s 0.0.0.0 -p 9002 -k $SSPASSWORD -m aes-256-cfb
+docker run -d --name container2 -p 9010:9010 oddrationale/docker-shadowsocks -s 0.0.0.0 -p 9010 -k $SSPASSWORD -m aes-256-cfb
 
-
-docker run -d -p 9010:9010 oddrationale/docker-shadowsocks -s 0.0.0.0 -p 9010 -k $SSPASSWORD -m aes-256-cfb
-
-docker run -d -p 9020:9020 oddrationale/docker-shadowsocks -s 0.0.0.0 -p 9020 -k $SSPASSWORD -m aes-256-cfb
 
