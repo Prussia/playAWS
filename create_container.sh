db0158c23bd1746627b0de726413c65df20ec3d9
Prussia <prussia.hu@pwc.com>
2019-10-10 16:07:55 +0800

    Update create_container.sh

--- create_container.sh
+++ create_container.sh
@@ -13,3 +13,8 @@ docker run -dt --name ssserver -p 6443:6443 -p 6500:6500/udp mritd/shadowsocks \
 -x -e "kcpserver" -k "-t 127.0.0.1:6443 -l :6500 -mode fast2"
 
 echo docker run -dt --name ssserver -p 6443:6443 -p 6500:6500/udp mritd/shadowsocks -m "ss-server" -s "-s 0.0.0.0 -p 6443 -m chacha20-ietf-poly1305 -k "$SSPASSWORD -x -e "kcpserver" -k "-t 127.0.0.1:6443 -l :6500 -mode fast2"
+
+
+docker run -dt --name trojan -p 443:443 trojangfw/trojan
+
+echo $ docker run -dt --name trojan -p 443:443 trojangfw/trojan
